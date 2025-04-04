8843cce01cedee02cac49d4fc2e633fad0e567ad
Mintesnot Girma <MintesnotGi@ethiopianairlines.com>
2025-04-04 12:25:48 +0300

    #

---
+++ UserManagmentService/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserManagmentService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Test API is working");
+        }
+        [HttpGet("test")]
+        public IActionResult Test()
+        {
+            return Ok("Test API is working");
+        }
+    }
+}
